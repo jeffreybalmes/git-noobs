6a0b24f30dae9ec45f0d41c25e3025ecd759e596
jeffreybalmes <jefbalmes@gmail.com>
2018-01-05 21:33:01 +0800

    initial updates

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var button = document.querySelector('button');
+var para = document.querySelector('p');
 
 button.addEventListener('click', titleColor);
 
 function titleColor() {
-    document.querySelector('h1').style.color = 'green';
+    document.querySelector('h1').style.color = 'deepskyblue';
+    para.style.color = 'deepskyblue';
 }
